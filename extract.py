819867e38fca5a4ff720e7172b7b54e0a6245dc4
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2020-05-09 10:04:26 -0300

    refactoring

--- extract.py
+++ extract.py
@@ -3,23 +3,28 @@ import pandas as pd
 import requests as r
 
 
-def consume_api(year, month, state_code, city_code, save_directory):
-    uri = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio/?mesAno={}&codigoIbge={}&pagina=1'.format(
-        (year+month), (state_code+city_code))
+def consume_api(year, month, state_code, city_code, save_path):
+    uri = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio/?mesAno={}{}&codigoIbge={}{}&pagina=1'.format(
+        year, month, state_code, city_code)
     response = r.get(uri)
 
     if response.status_code == 200:
-        with open(save_directory, mode='w', encoding='utf-8') as output:
+        with open(save_path, mode='w', encoding='utf-8') as output:
             temp = json.loads(response.text)
             json.dump(temp, output, ensure_ascii=False)
 
     return response.status_code
 
 
-def read_xls(file_directory, columns=[]):
-    df = pd.read_excel(file_directory)
+def read_xls(file_path, columns=[]):
+    df = pd.read_excel(file_path)
 
     if len(columns) > 0:
         df = [df[col] for col in columns]
 
     return df
+
+
+# Main:
+if __name__ == "__main__":
+    consume_api('2019', '01', '28', '00308', 'JSON/data.json')
