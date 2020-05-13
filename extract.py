6e8263b19379c0b40158587a8ad1f755638005b0
Rodolfo Brandão <rodolfobrandao1995@gmail.com>
2020-05-13 01:54:31 -0300

    get_ibge_codes function refactoring | replaced all single quotes to double ones to indicate that it's value is a string

--- extract.py
+++ extract.py
@@ -10,20 +10,21 @@ def get_date_range(year_range, month_range):
     for i in range(year_range[0], year_range[1]):
         for j in range(month_range[0], month_range[1]):
             dates.append(
-                '{}0{}'.format(i, j) if j <= 9 else '{}{}'.format(i, j))
+                "{}0{}".format(i, j) if j <= 9 else "{}{}".format(i, j))
 
     return dates
 
 
-def get_ibge_codes(file_path, filter_by_column):
+def get_ibge_codes(file_path, filter_by_column_value=""):
     ibge_codes = []
 
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
+    with open(file_path, newline='', encoding="utf-8") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
-        filtered = list(filter(lambda x: filter_by_column in x, spamreader))
+        filtered = list(filter(lambda x: filter_by_column_value in x, spamreader)
+                        ) if filter_by_column_value != "" else list(spamreader)
 
         for arr in filtered:
-            ibge_codes.append('{}{}'.format(arr[1], arr[2]))
+            ibge_codes.append("{}{}".format(arr[1], arr[2]))
 
     return ibge_codes
 
@@ -35,7 +36,7 @@ def gather_data(dates, ibge_codes, save_path):
     for code in ibge_codes:
         for date in dates:
             uris.append(
-                'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio/?mesAno={}&codigoIbge={}&pagina=1'.format(
+                "http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio/?mesAno={}&codigoIbge={}&pagina=1".format(
                     date, code
                 ))
 
@@ -45,7 +46,7 @@ def gather_data(dates, ibge_codes, save_path):
             if response.status_code == 200 and response.json():
                 returned_json.append(response.json()[0])
         except ValueError:
-            print('ValueError exception for: {}'.format(uri))
+            print("ValueError exception for: {}".format(uri))
         except Exception as e:
             blocked = True
             while blocked:
@@ -54,20 +55,19 @@ def gather_data(dates, ibge_codes, save_path):
 
             print(repr(e))
 
-    with open(save_path, 'w+', encoding='utf-8') as jsonfile:
+    with open(save_path, "w+", encoding="utf-8") as jsonfile:
         all_json = json.dumps(returned_json, indent=4, ensure_ascii=False)
         jsonfile.write(all_json)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     jobs = []
     dates = get_date_range((2016, 2020), (1, 13))
-    ibge_codes = get_ibge_codes('csv/municipios_IBGE.csv', 'SE')
+    ibge_codes = get_ibge_codes("csv/municipios_IBGE.csv", "SE")
 
     for i in range(0, 10):
         process = m.Process(target=gather_data, args=(
-            dates, ibge_codes, 'JSON/data.json'))
-
+            dates, ibge_codes, "JSON/data.json"))
         jobs.append(process)
 
     for job in jobs:
@@ -76,4 +76,4 @@ if __name__ == '__main__':
     for job in jobs:
         job.join()
 
-    print('Extraction process complete.')
+    print("Extraction process complete.")
