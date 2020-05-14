27f01d274833fc37dd3b74c7d5647f818a6bf876
Rodolfo <rodolfobrandao1995@gmail.com>
2020-05-14 03:56:53 -0300

    Refactored get_time_dimension() and implemented get_city_dimension() | get_fate_dimension() still in development

--- transform.py
+++ transform.py
@@ -33,36 +33,69 @@ state_regions = {
 }
 
 
-def get_time_dimension(year_range):
-    year_period = []
-
-    for year in range(year_range[0], year_range[1]):
-        for month in range(1, 13):
-            id = "{}0{}".format(
-                year, month) if month <= 9 else "{}{}".format(year, month)
-            bimonth = month // 2 if month % 2 == 0 else (month // 2) + 1
-            trimester = month // 3 if month % 3 == 0 else (month // 3) + 1
-            semester = month // 6 if month % 6 == 0 else (month // 6) + 1
-            year_period.append((id, year, month, bimonth, trimester, semester))
-
-    return year_period
-
-
-def get_city_dimension():
-    return
-
-
-if __name__ == "__main__":
-    year_period = get_time_dimension((2016, 2020))
-
-    with open("csv/time_dimension.csv", "w+", newline='') as csvfile:
+def get_time_dimension(year_range, month_range, save_path):
+    with open(save_path, "w+", newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow(
             ["id", "year", "month", "bimonth", "trimester", "semester"])
 
-        for period in year_period:
-            spamwriter.writerow(
-                [period[0], period[1], period[2], period[3], period[4], period[4]])
+        for year in range(year_range[0], year_range[1]):
+            for month in range(month_range[0], month_range[1]):
+                id = "{}0{}".format(
+                    year, month) if month <= 9 else "{}{}".format(year, month)
+                bimonth = month // 2 if month % 2 == 0 else (month // 2) + 1
+                trimester = month // 3 if month % 3 == 0 else (month // 3) + 1
+                semester = month // 6 if month % 6 == 0 else (month // 6) + 1
+
+                spamwriter.writerow(
+                    [id, year, month, bimonth, trimester, semester])
+
+
+def get_city_dimension(csv_file_path, save_path, column_value=""):
+    with open(save_path, "w+", newline='', encoding="utf-8") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["id", "city_name", "city_region", "state_name"])
+
+        with open(csv_file_path, newline='', encoding="utf-8") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            filtered = list(filter(lambda x: column_value in x, spamreader)
+                            ) if column_value != "" else list(spamreader)
+
+            for row in filtered:
+                id = "{}{}".format(row[1], row[2])
+                city_name = row[3]
+                city_region = state_regions[row[0]]
+                state_name = row[0]
+                spamwriter.writerow([id, city_name, city_region, state_name])
+
+
+def get_fate_dimension(json_file_path, save_path):
+    fate_dimension = []
+
+    with open(save_path, "w+", newline='', encoding="utf-8") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["TIME_DM_id", "CITY_DM_id",
+                             "beneficiaries_number", "amount_value"])
+
+        with open(json_file_path, "r", encoding="utf-8") as jsonfile:
+            json_data = json.load(jsonfile)
+            for key in json_data:
+                TIME_DM_id = key[7:]
+                CITY_DM_id = key[0:7]
+                beneficiaries_number = json_data[key]["quantidadeBeneficiados"]
+                amount_value = json_data[key]["valor"]
+
+                spamwriter.writerow(
+                    [TIME_DM_id, CITY_DM_id, beneficiaries_number, amount_value])
+
+
+if __name__ == "__main__":
+    get_time_dimension((2016, 2020), (1, 13), "csv/time_dimension.csv")
+    get_city_dimension("csv/municipios_IBGE.csv",
+                       "csv/city_dimension.csv", "SE")
+    get_fate_dimension("JSON/data.json", "csv/fate_dimension.csv")
 
     print("Transform process complete.")
