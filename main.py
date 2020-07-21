c88e3fbf6cd296725e3fb6545553ca1ecb24e9dd
Rodolfo <rodolfobrandao1995@gmail.com>
2020-07-21 02:25:46 -0300

    refactor: 'main' expression added

--- main.py
+++ main.py
@@ -3,32 +3,35 @@ import load as load_layer
 import transform as transform_layer
 import multiprocessing as m
 
-# Extract:
-jobs = []
-dates = extract_layer.set_date_range((2016, 2019), (1, 12))
-ibge_codes = extract_layer.get_ibge_codes("csv/municipios_IBGE.csv", "SE")
 
-for i in range(0, 10):
-    process = m.Process(target=extract_layer.gather_data, args=(
-        dates, ibge_codes, "JSON/data.json"))
-    jobs.append(process)
+if __name__ == "__main__":
+    # Extract:
+    jobs = []
+    dates = extract_layer.set_date_range((2016, 2019), (1, 12))
+    ibge_codes = extract_layer.get_ibge_codes("csv/municipios_IBGE.csv", "SE")
 
-for job in jobs:
-    job.start()
+    for i in range(0, 10):
+        process = m.Process(target=extract_layer.gather_data, args=(
+            dates, ibge_codes, "JSON/data.json"))
+        jobs.append(process)
 
-for job in jobs:
-    job.join()
+    for job in jobs:
+        job.start()
 
-print("Extraction process complete.")
+    for job in jobs:
+        job.join()
 
-# Transform:
-transform_layer.get_time_dimension(
-    (2016, 2020), (1, 13), "csv/time_dimension.csv")
-transform_layer.get_city_dimension("csv/municipios_IBGE.csv",
-                                   "csv/city_dimension.csv", "SE")
-transform_layer.get_fate_dimension("JSON/data.json", "csv/fate_dimension.csv")
+    print("Extraction process complete.")
 
-print("Transform process complete.")
+    # Transform:
+    transform_layer.get_time_dimension(
+        (2016, 2020), (1, 13), "csv/time_dimension.csv")
+    transform_layer.get_city_dimension("csv/municipios_IBGE.csv",
+                                       "csv/city_dimension.csv", "SE")
+    transform_layer.get_fate_dimension(
+        "JSON/data.json", "csv/fate_dimension.csv")
 
-# Load:
-# TODO
+    print("Transform process complete.")
+
+    # Load:
+    # TODO
