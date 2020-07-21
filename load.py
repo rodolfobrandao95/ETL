ece56392cc74d9815475e7431c5d7c9de4e67c12
Rodolfo <rodolfobrandao1995@gmail.com>
2020-07-20 22:20:45 -0300

    add/fix: moved all methods instances from the main statement (in each file) to main.py file

--- load.py
+++ load.py
@@ -1,18 +1,14 @@
-import pyodbc
+# import pyodbc
 
 
-server = "tcp:myserver.database.windows.net"
-database = "mydb"
-username = ""
-password = ""
-connection = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" +
-                            server+";DATABASE="+database+";UID="+username+";PWD=" + password)
-cursor = connection.cursor()
+# server = "tcp:myserver.database.windows.net"
+# database = "mydb"
+# username = ""
+# password = ""
+# connection = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" +
+#                             server+";DATABASE="+database+";UID="+username+";PWD=" + password)
+# cursor = connection.cursor()
 
 
 def get_DML_from_csv(file_path, save_path):
     return
-
-
-if __name__ == "__main__":
-    pass
---
+++ main.py
@@ -0,0 +1,30 @@
+import extract as extract_layer
+import transform as transform_layer
+import multiprocessing as m
+
+# Extraction:
+jobs = []
+dates = extract_layer.set_date_range((2016, 2019), (1, 12))
+ibge_codes = extract_layer.get_ibge_codes("csv/municipios_IBGE.csv", "SE")
+
+for i in range(0, 10):
+    process = m.Process(target=gather_data, args=(
+        dates, ibge_codes, "JSON/data.json"))
+    jobs.append(process)
+
+for job in jobs:
+    job.start()
+
+for job in jobs:
+    job.join()
+
+print("Extraction process complete.")
+
+# Transform:
+transform_layer.get_time_dimension(
+    (2016, 2020), (1, 13), "csv/time_dimension.csv")
+transform_layer.get_city_dimension("csv/municipios_IBGE.csv",
+                                   "csv/city_dimension.csv", "SE")
+transform_layer.get_fate_dimension("JSON/data.json", "csv/fate_dimension.csv")
+
+print("Transform process complete.")
