f2db54e928cd7b87c85d3e356467fc8aed7bf753
Rodolfo <rodolfobrandao1995@gmail.com>
2020-07-21 02:36:05 -0300

    refactor: replace all double quotes for single ones

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import transform as transform_layer
 import multiprocessing as m
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Extract:
     jobs = []
     dates = extract_layer.set_date_range((2016, 2019), (1, 12))
-    ibge_codes = extract_layer.get_ibge_codes("csv/municipios_IBGE.csv", "SE")
+    ibge_codes = extract_layer.get_ibge_codes('csv/municipios_IBGE.csv', 'SE')
 
     for i in range(0, 10):
         process = m.Process(target=extract_layer.gather_data, args=(
-            dates, ibge_codes, "JSON/data.json"))
+            dates, ibge_codes, 'JSON/data.json'))
         jobs.append(process)
 
     for job in jobs:
@@ -21,17 +21,17 @@ if __name__ == "__main__":
     for job in jobs:
         job.join()
 
-    print("Extraction process complete.")
+    print('Extraction process complete.')
 
     # Transform:
     transform_layer.get_time_dimension(
-        (2016, 2020), (1, 13), "csv/time_dimension.csv")
-    transform_layer.get_city_dimension("csv/municipios_IBGE.csv",
-                                       "csv/city_dimension.csv", "SE")
+        (2016, 2020), (1, 13), 'csv/time_dimension.csv')
+    transform_layer.get_city_dimension('csv/municipios_IBGE.csv',
+                                       'csv/city_dimension.csv', 'SE')
     transform_layer.get_fate_dimension(
-        "JSON/data.json", "csv/fate_dimension.csv")
+        'JSON/data.json', 'csv/fate_dimension.csv')
 
-    print("Transform process complete.")
+    print('Transform process complete.')
 
     # Load:
     # TODO
