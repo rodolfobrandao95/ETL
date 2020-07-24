8633758c376dc926a528fe6ab066feb4f1eae57f
Rodolfo <rodolfobrandao1995@gmail.com>
2020-07-23 21:03:40 -0300

    fix/chore: method name changed. generate_dml_from_csv method implemented

--- load.py
+++ load.py
@@ -1,3 +1,4 @@
+import csv
 # import pyodbc
 
 
@@ -10,5 +11,27 @@
 # cursor = connection.cursor()
 
 
-def get_DML_from_csv(file_path, save_path):
-    return
+def generate_dml_from_csv(reading_file: str, save_path: str, table_name: str, schema='') -> None:
+    '''
+    Converts a .csv file into a .sql file, with the
+    DML INSERT command, based on the table name specified.
+    Optional keywords arguments: schema: name of the database schema.
+    '''
+
+    prefix = ''
+    dml = ''
+
+    with open(save_path, 'w+', encoding='utf-8') as sqlfile:
+        with open(reading_file, newline='', encoding='utf-8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            headers = next(spamreader)
+
+            prefix = ('INSERT INTO [{}] VALUES ('.format(
+                table_name) if schema == '' else 'INSERT INTO [{}].[{}] ('.format(schema, table_name)) + ', '.join(headers) + ')\nVALUES ('
+
+            for columns in spamreader:
+                dml += prefix + \
+                    ', '.join(columns).replace("'", '', 1) + ');\n\n'
+
+        sqlfile.write(dml)
+    sqlfile.close()
